45de2c1f320205da099b4fd3441a2ff5f8521fa4
Jono Sommers <jonosommers95@gmail.com>
2024-10-24 19:25:54 -0500

    made hit test, test passing

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,38 +2,38 @@ require './lib/ship'
 
 RSpec.describe Ship do
 	before(:each) do
-		cruiser = Ship.new("Cruiser", 3)
+		@cruiser = Ship.new("Cruiser", 3)
 	end
 
   describe '#instantiate' do
 		it 'is a ship' do
-			expect(cruiser).to be_a(Ship)
+			expect(@cruiser).to be_a(Ship)
 		end
 	end
 
 	describe '#initalize' do
 		it 'has attributes' do
-			expect(cruiser.name).to eq("Cruiser")
-			expect(cruiser.length).to eq(3)
+			expect(@cruiser.name).to eq("Cruiser")
+			expect(@cruiser.length).to eq(3)
 		end
 	end
 
 	describe '#health' do
 		it 'has health' do
-			expect(cruiser.health).to eq(3)
+			expect(@cruiser.health).to eq(3)
 		end
 	end
 
 	describe '#sunk?' do
 		it 'starts as not sunk' do
-			expect(cruiser.sunk?).to be(false)
+			expect(@cruiser.sunk?).to be(false)
 		end
 	end
 
 	describe '#hit' do
 		it 'decreases ships health by one' do
-
-
+			@cruiser.hit
+			expect(@cruiser.health).to eq(2)
 		end
 	end
 end
