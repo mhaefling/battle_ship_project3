6fbdb0cceee2013fc9a20d71b2548b9b098de5e9
Jono Sommers <jonosommers95@gmail.com>
2024-10-27 15:04:42 -0500

    messing around with place_ships method

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,18 @@
 class Game
 
+    attr_reader :player_board, :computer_board
+
     def initialize
+        @player = {board: Board.new, cruiser: Ship.new("Cruiser", 3), submarine: Ship.new("Submarine", 2)}
+        @computer = {board: Board.new, cruiser: Ship.new("Cruiser", 3), submarine: Ship.new("Submarine", 2)}
+        require 'pry'; binding.pry
     end
 
     def start_game(user_input)
         if user_input == 'p'
             puts "Game Started."
+            @player_board
+            @computer_board
 
         elsif user_input == 'q'
             puts "Game canceled."
@@ -14,4 +21,10 @@ class Game
         end
     end
 
+    def place_ships(ship, coordinates)
+        3.times @computer[:board].cells.keys.sample do
+        until valid_placement? == true
+            false
+        end
+    end
 end
