cabd8c16cb7a817318cd3035f9adcdef830f4ae0
Matt Haefling <m.haefling@protonmail.com>
2024-10-27 18:06:27 -0600

    added player_place_ships method, still working out some bugs

--- lib/game.rb
+++ lib/game.rb
@@ -18,8 +18,13 @@ class Game
 
     def start_game(user_input)
         if user_input == 'p'
-            puts "Please wait while the other player places their ships."
+            puts "Please wait while the computer places its ships."
             computer_place_ships
+            puts " "
+            puts """I have laid out my ships on the grid.\nYou now need to lay out your two ships.\nThe Cruiser is three units long and the Submarine is two units long."""
+            puts " "
+            puts @player["board"].render
+
 
         elsif user_input == 'q'
             puts "Game canceled."
@@ -50,6 +55,16 @@ class Game
                 random_cords.clear
             end
         end
-        puts @computer["board"].render(true)
+    end
+
+    def player_place_ships(user_placement)
+        player_cords = user_placement.split(" ")
+        validate = @player["board"].valid_placement?(@player["cruiser"], player_cords)
+        if validate == true
+            @player["board"].place(@player["cruiser"], player_cords)
+        else
+            player_cords.clear
+            false
+        end
     end
 end
